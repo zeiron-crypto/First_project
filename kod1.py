308f5db26df3aaa6dad76eb199a6de48597ecf70
vladislavkan <56309007+vladislavkan@users.noreply.github.com>
2019-12-03 17:01:59 +0200

    Add files via upload

---
+++ kod1.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.plot([0],[0], color='g', marker='o', ms=10, label ='планета1')
+plt.plot([0],[2.8*10**12], color='y', marker='o', ms=25,label ='планета2')
+plt.axis('equal')
+
+y0 = 142*10**10
+d = 2.5*y0
+alpha = 30
+d1 = 4*y0
+
+
+
+xt = d*np.sin(alpha*np.pi/180)
+yt = d*np.cos(alpha*np.pi/180)
+plt.plot([0,xt], [0,yt],'c--')
+
+d3 = 5*y0
+
+xtt = d3*np.sin(alpha*np.pi/180)
+ytt = d3*np.cos(alpha*np.pi/180)
+plt.plot([0,xtt], [0,ytt],'c--',label ='искривлённый луч')
+
+
+def f(M=5.9726*10**34,alpha=30):
+    r = y0*np.tan(alpha*np.pi/180)
+    #M = 5.9726*10**34
+    G = 6.67408*10**(-11) 
+    C = 3*10**8
+    b = (4*G*M)/(r*(C**2))
+    u = (90 + alpha)*np.pi/180 - b
+    #print(b)
+    xx = d1*np.cos(u)
+    yx = d1*np.sin(u)
+    plt.plot([xx,xt], [yx,yt],label ='луч')
+    plt.xlim(0,15)
+    plt.ylim(0,15)
+    plt.axis('equal')
+    plt.legend()
+    plt.show()
+f()
